6513b0a82d08042356fbe7e71f41b467f8aba071
DeCoder <rao.sush004@gmail.com>
2020-10-05 11:23:30 +0530

    Fix multiple requests

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -18,9 +18,15 @@ $(document).ready(function () {
 });
 /* Script to lazy load posts using pagination URLs */
 function lazyLoad() {
+    if (window.pagination_loading) {
+        return ;
+    }
     let more_posts_url = $('#paginate-infinite-scrolling .pagination .next_page a').attr('href');
-    if (more_posts_url && ($(window).scrollTop() > $(document).height() - $(window).height() - 30)) {
+    if (more_posts_url && ($(window).scrollTop() > $(document).height() - $(window).height() - 60)) {
+        window.pagination_loading = true;
         $('#ajax-loader').html('<img src="/ajax-loader.gif" alt="Loading..." title="Loading..." />');
-        $.getScript(more_posts_url);
+        $.getScript(more_posts_url).always(function () {
+            window.pagination_loading = false;
+        });
     }
 }
